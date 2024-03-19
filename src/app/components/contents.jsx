1621d2defb30579d87ce4492476a55a7084e7957
Riobush32 <riobush32@gmail.com>
2024-03-19 22:20:13 +0700

    add tailwind icon

--- src/app/components/contents.jsx
+++ src/app/components/contents.jsx
@@ -13,11 +13,11 @@ const Contents = () => {
                         <div className="">
                             <h1 className="text-2xl font-light">Hello!</h1>
                             <p className="py-6 text-5xl font-semibold  md:leading-3">I AM RIO SOFHANIEL BUSH </p>
-                            <p className="text-secondary md:leading-5">Frond- End Programmer || Back-End Programemer || Full-Stack Programmer </p>
-                            <button className="btn btn-sm btn-secondary mt-3">
+                            <p className="text-secondary md:leading-5">Frond- End Programmer || Back-End Programmer || Full-Stack Programmer </p>
+                            <a href="https://mail.google.com/mail/u/0/?view=cm&tf=1&fs=1&to=riobush32@gmail.comg" className="btn btn-sm btn-secondary mt-3">
                                 Let's Talk
                                 <svg xmlns="http://www.w3.org/2000/svg" width={15} viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" /></svg>
-                            </button>
+                            </a>
                             
                         </div>
                         <div className="min-w-32 absolute bottom-0">
@@ -43,6 +43,7 @@ const Contents = () => {
                                     </a>
                                     
                                 </div>
+                                
                             </div>
                             <a href="/assets/cv.pdf" download className="ml-1 text-sm text-white" ><span className="text-secondary">Download</span> curriculum vitae</a>
                         </div>
